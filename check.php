961ea20af4fd7a911544acf123617313e4ca3609
カーズ <magmanakazato@gmail.com>
2018-10-08 20:21:16 +0800

    phpの処理をファイル別にした（作成したファイル：db_check.php）

--- check.php
+++ check.php
@@ -2,88 +2,9 @@
 
     session_start();
 
-// ----ここからdbconnect.phpファイルにする-------
-
-    $dsn = 'mysql:dbname=nexstage_test;host=localhost';
-
-    $user ='root';
-    $password = '';
-    $dbh = new PDO($dsn, $user, $password);
-    // SQL文にエラーがあった場合、画面にエラーを出力する設定
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-
-    // 文字コードの指定
-    $dbh->query('SET NAMES utf8');
-
-// ----ここまでdbconnect.phpファイルにする-------
-
-
-
-    echo '<pre>';
-    var_dump($_POST);
-    echo '</pre>';
-
-    echo '<pre>';
-    var_dump($_SESSION['nexstage_test']);
-    echo '</pre>';
-
-    $check = '';
-
-    // echo $_SESSION['nexstage_test']['user_name'];
-
-    if (!isset($_SESSION['nexstage_test'])) {
-        // sign_inへ強制遷移させる
-        header('Location: sign_in_logic.php');
-    }
-
-    $user_name = $_SESSION['nexstage_test']['user_name'];
-    $user_id = $_SESSION['nexstage_test']['user_id'];
-    $email = $_SESSION['nexstage_test']['email'];
-    $user_password = $_SESSION['nexstage_test']['signup_password'];
-
-    if (!empty($_POST)) {
-        echo "<br>" . "<br>";
-        echo "通過";
-
-        $hash_password = password_hash($user_password, PASSWORD_DEFAULT);
-        // $hash_password = $user_password;
-
-
-// ----ここからdbinsert.phpファイルにする-------
-
-        // img_nameありSQL文
-        // $sql = 'INSERT INTO `users` SET `user_name` = ?, `user_id` = id, `email` = ?, `password` = ?, `img_name` = ?, `created` = NOW()';
-
-        // $data = [$user_name, $user_id, $email, $password, $img_name];
-
-        // img_nameなしSQL文
-        $sql = 'INSERT INTO `users` SET `user_name` = ?, `user_id` = ?, `email` = ?, `password` = ?, `created` = NOW()';
-
-        $data = [$user_name, $user_id, $email, $hash_password];
-
-        $stmt = $dbh->prepare($sql);
-        $stmt->execute($data);
-
-        // DB切断
-        $dbh = null;
-
-// ----ここからdbinsert.phpファイルにする-------
-
-        // 登録完了ページへ遷移
-        header('Location: thanks.php');
-
-        if ($check == 'back') {
-        	header('Location: signup_and_in.php');
-        }
-
-    }
-
+    require_once('../dbconnect/db_check.php');
 
 ?>
-
-
 
 <!DOCTYPE html>
 <html>
