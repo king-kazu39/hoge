084f175d00ce0143dbf280a5049700b1934b5b65
北野千裕 <chi.t.angel.s2@gmail.com>
2018-10-22 18:18:02 +0800

    いいね機能作成のため新しいファイル作成しました。

---
+++ js/app.js
@@ -0,0 +1,39 @@
+
+
+$(function() {
+
+    $('.js-like').on('click', function() {
+
+
+        var target_id = $(this).siblings('.target-id').text();
+        var user_id = $(this).siblings('.user-id').text();
+        console.log(target_id);   //target_idを取得できているか確認
+        console.log(user_id);   //user_idを取得できているか確認            
+
+
+
+
+        $.ajax({
+            // 送信先、送信するデータなど
+            url: 'like.php',
+            type: 'POST',
+            datatype: 'json',
+            data: {
+                'target-id': target_id,
+                'user-id': user_id,
+            }
+
+
+        })
+        .done(function(data) {
+            // 成功時の処理
+            console.log(data);
+        })
+        .fail(function(err) {
+            // 失敗時の処理
+            console.log('error');
+        })
+
+    });
+
+});
