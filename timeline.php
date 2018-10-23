1e3a3b275856c54b51559e1deabe1add5e4d5a3d
北野千裕 <chi.t.angel.s2@gmail.com>
2018-10-23 15:54:26 +0800

    85c3e674fff8a2630c774d034f95f32700281657

--- timeline.php
+++ timeline.php
@@ -8,8 +8,8 @@
 
 
 	// TODO: ID仮打ち→OK
-	// $signin_user_id = $_SESSION['nexstage_test']['id'];
-	$signin_user_id = 5;
+	$signin_user_id = $_SESSION['nexstage_test']['id'];
+	// $signin_user_id = 5;
 
 
 // =====================ここからユーザ名とユーザプロフィール画像取得=====================
@@ -125,7 +125,7 @@
         $record['comments'] = get_comments($dbh, $record['id']);
         // コメント数を取得
         $record["comment_cnt"] = count_comments($dbh, $record['id']);
-        // =====================コメント一覧=======================
+// =====================コメント一覧=======================
 
         // レコードがあれば追加
         $feeds[] = $record;
@@ -255,8 +255,8 @@
                     </div><!--menu-btn end-->
                     <div class="user-account">
                         <div class="user-info">
-                            <img src="http://via.placeholder.com/30x30" alt="">
-                            <a href="my-profile.php" title="">井上　侑弥</a>
+                            <img src="user_profile_img/<?php echo $user['img_name']; ?>" width = '30' height="30" alt="">
+                            <a href="my-profile.php" style="width:60px; height:20px; font-size: 20px; title=""><?php echo $user['name']; ?></a>
                         </div>
                     </div>
                     <div class="search-bar">
@@ -403,7 +403,7 @@
                                 <div class="main-ws-sec">
                                     <div class="post-topbar">
                                         <div class="user-picy">
-                                            <img src="http://via.placeholder.com/100x100" alt="">
+                                            <img src="user_profile_img/<?php echo $user['img_name']; ?>" alt="">
                                         </div>
                                         <div class="post-st">
                                             <ul>
@@ -461,15 +461,16 @@
                                                         <span>: </span>
                                                         <span class="like_count">10</span>
 <!-- ===========================ここまでいいね機能実装===============================================- -->
+<!-- ======================コメント機能============================================================== -->
+						
+						<a href="#collapseComment<?= $feed["id"] ?>" data-toggle="collapse" aria-expanded="false">
+                                    <span>コメント</span>
+                                </a>
+                                <span class="comment_count">: <?= $feed["comment_cnt"] ?></span>
 
-<!-- ======================コメント機能=========================== -->
-                                                        <a href="#collapseComment<?= $feed["id"] ?>" data-toggle="collapse" aria-expanded="false">
-                                                            <span>コメント</span>
-                                                        </a>
-                                                        <span class="comment_count">: <?= $feed["comment_cnt"] ?></span>
-                                                        <br>
-                                                        <?php include('comment_view.php'); ?>
-                                                    </div>
+                                <br>
+                                <?php include('comment_view.php'); ?>
+                    </div>
 <!-- ========================================ここまでコメント機能=========================================== -->
                                                 </ul>
                                             </div>
