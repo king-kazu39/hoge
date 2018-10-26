49380e22d5f232f32740f1d1e00af5cacf6ad197
koki <k.koki0426@gmail.com>
2018-10-26 16:01:59 +0800

    heroku用にDBの設定変更

--- dbconnect/dbconnect.php
+++ dbconnect/dbconnect.php
@@ -1,9 +1,12 @@
 <?php
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-    $dsn = 'mysql:dbname=nexstage_test;host=localhost';
+    // $dsn = 'mysql:dbname=nexstage_test;host=localhost';
+    $dsn = 'mysql:dbname=' . substr($url['path'], 1) . ';host=' . $url['host'];
 
-    $user ='root';
-    $password = '';
+    // $user ='root';
+    $user =$url['user'];
+    $password = $url['pass'];
     $dbh = new PDO($dsn, $user, $password);
     // SQL文にエラーがあった場合、画面にエラーを出力する設定
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
