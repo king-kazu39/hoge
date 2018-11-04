7008a370df3a9c336029ee85b384c37a79deefbb
koki <k.koki0426@gmail.com>
2018-11-04 16:19:14 +0900

    DBローカル用に変更

--- dbconnect/dbconnect.php
+++ dbconnect/dbconnect.php
@@ -1,13 +1,15 @@
 <?php
 	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-    // $dsn = 'mysql:dbname=nexstage_test;host=localhost';
-    $dsn = 'mysql:dbname=' . substr($url['path'], 1) . ';host=' . $url['host'];
+    $dsn = 'mysql:dbname=nexstage_test;host=localhost';
+    //$dsn = 'mysql:dbname=' . substr($url['path'], 1) . ';host=' . $url['host'];
 
-    // $user ='root';
-    $user =$url['user'];
-    $password = $url['pass'];
+    $user ='root';
+    //$user =$url['user'];
+    //$password = $url['pass'];
+    $password = '';
     $dbh = new PDO($dsn, $user, $password);
+   
     // SQL文にエラーがあった場合、画面にエラーを出力する設定
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
